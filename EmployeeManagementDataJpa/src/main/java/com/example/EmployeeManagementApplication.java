7babe0925ca9388f10e164cd7d60b3bb2904db7d
omkar <omkarchoramale03@gmail.com>
2026-03-13 14:44:32 +0530

    first commit

--- EmployeeManagementDataJpa/src/main/java/com/example/EmployeeManagementApplication.java
+++ EmployeeManagementDataJpa/src/main/java/com/example/EmployeeManagementApplication.java
@@ -8,6 +8,7 @@ public class EmployeeManagementApplication {
 
 	public static void main(String[] args) {
 		System.out.println("first line after git used");
+		System.out.println("second line after git used");
 		SpringApplication.run(EmployeeManagementApplication.class, args);
 	}
 
